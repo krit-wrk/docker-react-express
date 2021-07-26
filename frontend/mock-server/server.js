e5db0805696ee8542b40f4d562c8b5f3608667ad
Krit <krit.wrk@gmail.com>
2021-07-26 15:29:40 +0700

    add todo logic

--- frontend/mock-server/server.js
+++ frontend/mock-server/server.js
@@ -11,6 +11,15 @@ const app = express();
 app.get("/api/hello", (req, res) => {
   res.send("Hello World From Server");
 });
+app.get("/api/todos", (req, res) => {
+  res.send([
+    { _id: 1, text: "todo01" },
+    { _id: 2, text: "todo02" },
+  ]);
+});
+app.post("/api/todos", (req, res) => {
+  res.send({ _id: 3, text: "todo03" });
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const onLanding = async () => {
+    const res = await axios.get("./api/todos");
+    setTodoList(res.data);
+  };
+
+  useEffect(() => {
+    onLanding();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button
-          onClick={async () => {
-            const res = await axios.get("./api/hello");
-            setText(res.data);
-          }}
-        >
-          Call hello
-        </button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {text}
-        </a>
-      </header>
+      <button
+        onClick={async () => {
+          const res = await axios.get("./api/hello");
+          setText(res.data);
+        }}
+      >
+        Call hello
+      </button>
+      {text}
+      <div>
+        <div>
+          <input
+            value={todo}
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
+          />
+          <button
+            onClick={async () => {
+              const res = await axios.post("./api/todos", { text: todo });
+              setTodo("");
+            }}
+          >
+            add
+          </button>
+        </div>
+        {todoList.map((t) => (
+          <div key={t._id}>{t.text}</div>
+        ))}
+      </div>
     </div>
   );
 }

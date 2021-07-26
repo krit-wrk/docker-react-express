18b213e3e0ade1e3c939eb451f14497f3f7cf5a1
Krit <krit.wrk@gmail.com>
2021-07-26 14:25:36 +0700

    add docker prod

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,46 @@
 "use strict";
 
 const express = require("express");
+const { MongoClient } = require("mongodb");
 
 // Constants
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
-// App
-const app = express();
-app.get("/api/hello", (req, res) => {
-  res.send("Hello World From Server");
-});
+// Connection URL
+const url = "mongodb://mongo:27017";
+const client = new MongoClient(url);
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+// Database Name
+const dbName = "myProject";
+
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("documents");
+
+  const app = express();
+  app.get("/api/hello", (req, res) => {
+    res.send("Hello World From Server");
+  });
+  app.get("/api/documents", async (req, res) => {
+    const data = await collection.find();
+    res.send(data);
+  });
+  app.post("/api/documents", async (req, res) => {
+    console.log("/api/documents");
+    const insertResult = await collection.insertMany([
+      { a: 1 },
+      { a: 2 },
+      { a: 3 },
+    ]);
+    console.log("Inserted documents =>", insertResult);
+    res.send("Inserted documents");
+  });
+
+  app.listen(PORT, HOST);
+  console.log(`Running on http://${HOST}:${PORT}`);
+}
+throw Error("asd");
+main();
